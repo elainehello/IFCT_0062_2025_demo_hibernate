199e6012568a302355bcc5858d1d909d94ecdbfe
elainehello <195331169+elainehello@users.noreply.github.com>
2025-05-07 20:55:57 +0200

    configuring JavaBean and HibernateUtil

--- src/main/java/com/elainevalles/demo/model/Videojuego.java
+++ src/main/java/com/elainevalles/demo/model/Videojuego.java
@@ -1,35 +1,41 @@
 package com.elainevalles.demo.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 
 @Entity
 @Table(name="tvideojuego")
 public class Videojuego {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String title;
     private String platform;
     private boolean multiplayer;
+    @Column(name="pvp")
     private BigDecimal price;
+    @Transient // Doesn't get saved to the database' the field is transient the var status
+    private boolean status;
 
     public Videojuego() { // empty constructor for JPA
     }
 
-    public Videojuego(String title, String platform, boolean multiplayer, BigDecimal price) { // Doesnt need an ID
-        this.title = title;
-        this.platform = platform;
-        this.multiplayer = multiplayer;
+    public Videojuego(boolean status, BigDecimal price, boolean multiplayer, String platform, String title) { // Doesn't need an ID'
+        this.status = status;
         this.price = price;
+        this.multiplayer = multiplayer;
+        this.platform = platform;
+        this.title = title;
     }
 
-    public Videojuego(int id, String title, String platform, boolean multiplayer, BigDecimal price) { // Need an ID and all the other fields
+    public Videojuego(int id, String title, String platform, boolean multiplayer, BigDecimal price, boolean status) { // Need an ID and all the other fields
         this.id = id;
         this.title = title;
         this.platform = platform;
         this.multiplayer = multiplayer;
         this.price = price;
+        this.status = status;
     }
 
     public int getId() {
@@ -70,5 +76,13 @@ public class Videojuego {
 
     public void setPrice(BigDecimal price) {
         this.price = price;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
     }
 }
